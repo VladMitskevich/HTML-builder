714fce40badd5d1b022254e9a60ee6d14b61fd18
Vlad Mitskevich <vlad.mitskevich@mail.ru>
2022-05-23 15:31:45 +0300

    feat: add 1 and 2 parts of task

---
+++ 02-write-file/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+fs.writeFile(path.join(__dirname, 'text.txt'), ' ',
+  (err) => {
+  if (err) throw err;
+  console.log('Write text, please');
+}
+);
+readline.on('line', data => {
+    writeStream.write(`${data}\n`, err => {
+      if (err) throw err;
+    });
+    if (data.trim() === 'exit') {
+    readline.close();
+  }
+});
+
+
+process.on('exit', code => {
+  if (code === 0) {
+    console.log('Thanks for your attention!');
+  } else {
+    console.log(`Error with ${code}`);
+  }
+});
+readline.on('close', () => {
+  process.exit();
+});
+process.on('SIGINT', () => {
+  process.exit();
+});
